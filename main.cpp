1ad837f0705ac7b86439ecfe1c142301222545d3
mobinsheng <mobinsheng@mobinshengdeMac-mini.local>
2019-05-27 11:38:02 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+
+#define WEBRTC_MAC 1 //必须要定义
+#include "byteorder.h"
+
+int main()
+{
+    if(rtc::IsHostBigEndian()){
+        cout << "BigEndian" << endl;
+    }
+    else{
+        cout << "LittleEndian" << endl;
+    }
+
+    return 0;
+}
